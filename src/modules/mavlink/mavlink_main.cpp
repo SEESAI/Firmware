aa5759468d7b41692ba458c29bc74fff5313302e
Edu <ealdaz@sees.ai>
2020-09-07 19:15:07 +0100

    OB control from RC Tx enabled an ported to PX4 V1.10.1

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1604,7 +1604,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 	case MAVLINK_MODE_NORMAL:
 		configure_stream_local("ADSB_VEHICLE", unlimited_rate);
 		configure_stream_local("ALTITUDE", 1.0f);
-		configure_stream_local("ATTITUDE", 15.0f);
+		configure_stream_local("ATTITUDE", 5.0f);
 		configure_stream_local("ATTITUDE_TARGET", 2.0f);
 		configure_stream_local("BATTERY_STATUS", 0.5f);
 		configure_stream_local("CAMERA_IMAGE_CAPTURED", unlimited_rate);
@@ -1630,9 +1630,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("RC_CHANNELS", 5.0f);
 		configure_stream_local("SERVO_OUTPUT_RAW_0", 1.0f);
 		configure_stream_local("SYS_STATUS", 1.0f);
-		configure_stream_local("UTM_GLOBAL_POSITION", 0.5f);
-		configure_stream_local("VFR_HUD", 4.0f);
-		configure_stream_local("WIND_COV", 0.5f);
+		//configure_stream_local("UTM_GLOBAL_POSITION", 0.5f);
+		//configure_stream_local("VFR_HUD", 4.0f);
+		//configure_stream_local("WIND_COV", 0.5f);
 		break;
 
 	case MAVLINK_MODE_ONBOARD:
