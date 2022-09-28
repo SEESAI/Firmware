5f1b6d6c4d666de3065058e857f300681ef9cc72
ealdaz-seesai <35691522+ealdaz-seesai@users.noreply.github.com>
2022-09-28 11:19:25 +0100

    Increased rate of RC channels and adjusted Attitude, Global pos and Altitude.

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1705,15 +1705,15 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 	/* fallthrough */
 	case MAVLINK_MODE_CUSTOM:
 		//stream nothing
-		configure_stream_local("ALTITUDE", 0.5f);
-		configure_stream_local("ATTITUDE", 5.0f);
+		configure_stream_local("ALTITUDE", 3.0f);
+		configure_stream_local("ATTITUDE", 3.0f);
 		configure_stream_local("BATTERY_STATUS", 0.5f);
 		configure_stream_local("EXTENDED_SYS_STATE", 0.1f);
-		configure_stream_local("GLOBAL_POSITION_INT", 5.0f);
+		configure_stream_local("GLOBAL_POSITION_INT", 3.0f);
 		configure_stream_local("GPS_RAW_INT", 0.5f);
 		configure_stream_local("HOME_POSITION", 0.1f);
 		configure_stream_local("NAMED_VALUE_FLOAT", 1.0f);
-		configure_stream_local("RC_CHANNELS", 0.5f);
+		configure_stream_local("RC_CHANNELS", 2.0f);
 		configure_stream_local("SYS_STATUS", 0.1f);
 		configure_stream_local("VFR_HUD", 1.0f);
 
