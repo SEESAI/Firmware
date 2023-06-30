5ac2a765a423781bf7c5c1f309950bcd7875abdd
DavidP <dpatrick@sees.ai>
2023-06-30 15:35:35 +0100

    Testing fix

--- src/drivers/telemetry/frsky_telemetry/sPort_data.cpp
+++ src/drivers/telemetry/frsky_telemetry/sPort_data.cpp
@@ -200,7 +200,7 @@ void sPort_send_BATV(int uart)
 void sPort_send_CUR(int uart)
 {
 	/* Hijacked to send Data Source type (Mav/RC Control)(David @sees.ai) */
-	uint32_t control_source = s_port_subscription_data->manual_control_setpoint_sub.get().data_source;
+	int32_t control_source = s_port_subscription_data->manual_control_setpoint_sub.get().data_source;
 	bool control_source_valid = s_port_subscription_data->manual_control_setpoint_sub.get().valid;
 
 	if (!control_source_valid) {
@@ -417,7 +417,7 @@ void sPort_send_DIY_gps_mb(int uart)
 void sPort_send_DIY_rcmav(int uart)
 {
 	// OBManual Control Mode
-	uint32_t control_source = s_port_subscription_data->manual_control_setpoint_sub.get().data_source;
+	int32_t control_source = s_port_subscription_data->manual_control_setpoint_sub.get().data_source;
 	bool control_source_valid = s_port_subscription_data->manual_control_setpoint_sub.get().valid;
 
 	if (!control_source_valid) {
@@ -435,6 +435,7 @@ void sPort_send_DIY_rcmav(int uart)
 		control_source = manual_control_setpoint_s::SEES_SOURCE_MAV; // This equates to 1
 	}
 
+	PX4_INFO("value = %i", control_source);
 	sPort_send_data(uart, SMARTPORT_ID_RCMAV, control_source);
 }
 
