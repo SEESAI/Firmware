112b26c9e0bdaab48f7fabf9d6921bae15b36c41
DavidP <dpatrick@sees.ai>
2023-07-05 14:47:31 +0100

    Format

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -73,8 +73,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic("irlock_report", 1000);
 	add_optional_topic("landing_target_pose", 1000);
 	add_optional_topic("magnetometer_bias_estimate", 200);
-	add_topic("magnetometer_noise",
-		  200);				// Sees.ai - Added topic for monitoring mag disturbance and filter performance (e.g pylons)
+	add_topic("magnetometer_noise", 200);				// Sees.ai - Added topic for monitoring mag disturbance/filter performance
 	add_topic("manual_control_setpoint", 200);
 	add_topic("manual_control_switches");
 	add_topic("mission_result");
