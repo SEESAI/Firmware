e14a0724edb7e5823259088b573d27b63c7ba1e7
DavidP <dpatrick@sees.ai>
2024-02-08 12:48:23 +0000

    Removed Edu's work

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -1768,8 +1768,6 @@ void Commander::executeActionRequest(const action_request_s &action_request)
 			const char kill_switch_string[] = "Kill-switch engaged\t";
 			events::LogLevels log_levels{events::Log::Info};
 
-			// set_tune(tune_control_s::TUNE_ID_BATTERY_WARNING_FAST);  // Edu added for BVLOS compliance
-
 			if (_vehicle_land_detected.landed) {
 				mavlink_log_info(&_mavlink_log_pub, kill_switch_string);
 
