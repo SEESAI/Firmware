319cdf8d83524f5b9d79afcb880edfc6c5a69b88
rssmtj <m.rossi@live.com>
2024-08-28 15:35:23 +0100

    Offboard on position loss (#88)
    * Commented check, works in SITL. TODO: verify unintended consequences
    
    * Running make format
    
    * Added comment explaining the change
    
    ---------
    
    Co-authored-by: DavidP <dpatrick@sees.ai>

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -4367,10 +4367,16 @@ Commander::offboard_control_update()
 	} else if (_offboard_control_mode_sub.get().velocity && !_status_flags.local_velocity_valid) {
 		offboard_available = false;
 
-	} else if (_offboard_control_mode_sub.get().acceleration && !_status_flags.local_velocity_valid) {
-		// OFFBOARD acceleration handled by position controller
-		offboard_available = false;
 	}
+
+	// ---sees.ai--- 13/8/2024
+	// Disabled this check to allow SI2 to keep controlling the drone if GPS is lost.
+	// This change is part of implementing the "position lost" mode in SI2.
+	//
+	// else if (_offboard_control_mode_sub.get().acceleration && !_status_flags.local_velocity_valid) {
+	// 	// OFFBOARD acceleration handled by position controller
+	// 	offboard_available = false;
+	// }
 
 	_offboard_available.set_state_and_update(offboard_available, hrt_absolute_time());
 
