bdb03012890e82b1527cffefd1a17b9003c53896
William Solichin <wsolichin@sees.ai>
2026-08-19 16:57:06 +0100

    Fix typo

--- src/drivers/uavcan/sensors/gnss.cpp
+++ src/drivers/uavcan/sensors/gnss.cpp
@@ -302,7 +302,6 @@ UavcanGnssBridge::gnss_fix2_sub_cb(const uavcan::ReceivedDataStructure<uavcan::e
 	int32_t noise_per_ms = -1;
 	int32_t jamming_indicator = -1;
 	uint8_t jamming_state = 0;
-	uint8_t spoofing_state = 0;
 
 	// Use ecef_position_velocity for now... There is no heading field
 	if (!msg.ecef_position_velocity.empty()) {
@@ -316,7 +315,7 @@ UavcanGnssBridge::gnss_fix2_sub_cb(const uavcan::ReceivedDataStructure<uavcan::e
 			heading_accuracy = msg.ecef_position_velocity[0].velocity_xyz[2];
 		}
 		
-		noise_per_ms = msg.ecef.position_velocity[0].position_xyz_mm[0];
+		noise_per_ms = msg.ecef_position_velocity[0].position_xyz_mm[0];
 		jamming_indicator = msg.ecef_position_velocity[0].position_xyz_mm[1];
 		
 		jamming_state = msg.ecef_position_velocity[0].position_xyz_mm[2] >> 8;
