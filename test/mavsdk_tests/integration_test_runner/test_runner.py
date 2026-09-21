8e2fd8589d10ba5dba9b1756ddad5e510eefe9c1
Vilas Chitrakaran <vchitrakaran@sees.ai>
2026-09-21 14:25:54 +0100

    ci: run firmware CI jobs on GitHub-hosted runners  (#113)
    * ci: run firmware CI jobs on GitHub-hosted runners
    * ci: fix ros2 integration tests
    * ci: Disable CI on experimental board px4_fmu-v6c_neural
    * ci: fixes sitl tests to run on our github runners

--- test/mavsdk_tests/integration_test_runner/test_runner.py
+++ test/mavsdk_tests/integration_test_runner/test_runner.py
@@ -458,20 +458,23 @@ class Tester:
             "public": "true"
         }
 
-        with open(ulog_path, 'rb') as f:
-            r = requests.post(server + "/upload",
-                              data=payload,
-                              files={'filearg': f},
-                              allow_redirects=False)
-            if r.status_code == 302:  # redirect
-                if 'Location' in r.headers:
-                    plot_url = r.headers['Location']
-                    if len(plot_url) > 0 and plot_url[0] == '/':
-                        plot_url = server + plot_url
-                    print("    Uploaded to: " + plot_url)
-            else:
-                print("    Upload failed with status_code: {}"
-                      .format(r.status_code))
+        try:
+            with open(ulog_path, 'rb') as f:
+                r = requests.post(server + "/upload",
+                                  data=payload,
+                                  files={'filearg': f},
+                                  allow_redirects=False)
+                if r.status_code == 302:  # redirect
+                    if 'Location' in r.headers:
+                        plot_url = r.headers['Location']
+                        if len(plot_url) > 0 and plot_url[0] == '/':
+                            plot_url = server + plot_url
+                        print("    Uploaded to: " + plot_url)
+                else:
+                    print("    Upload failed with status_code: {}"
+                          .format(r.status_code))
+        except Exception as e:
+            print("    Upload failed with exception: {}".format(e))
 
     def start_combined_log(self, filename: str) -> None:
         self.log_fd = open(filename, 'w')
